8e21f68086a06c69c2726de485ec481bce077de3
michalellouche20 <michalelouche@outlook.com>
2024-01-01 20:57:42 +0200

    added dc

---
+++ week6-JS/day3/dc/script.js
@@ -0,0 +1,31 @@
+let client = "John";
+
+const groceries = {
+    fruits: ["pear", "apple", "banana"],
+    vegetables: ["tomatoes", "cucumber", "salad"],
+    totalPrice: "20$",
+    other: {
+        paid: true,
+        meansOfPayment: ["cash", "creditCard"]
+    }
+};
+
+const displayGroceries = () => {
+    groceries.fruits.forEach(fruit => console.log(fruit));
+};
+
+const cloneGroceries = () => {
+    let user = client; 
+    client = "Betty";
+
+    let shopping = { ...groceries }; 
+    shopping.totalPrice = "35$"; 
+    shopping.other.paid = false; 
+
+    console.log("User:", user);
+    console.log("Shopping:", shopping);
+};
+
+displayGroceries();
+
+cloneGroceries();
